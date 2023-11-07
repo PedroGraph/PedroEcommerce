091402dd4573f32c30e495bc4812be4ee3edd0e1
Pmedinam <p.medina@sell2rent.com>
2023-11-07 16:24:14 -0500

    Fixing database

--- mainproyect/settings.py
+++ mainproyect/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
+if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
