c53251189ef179bc465b19a6048e5bb2c2dc7eab
Pmedinam <p.medina@sell2rent.com>
2023-11-07 16:16:58 -0500

    Fixing database

--- mainproyect/settings.py
+++ mainproyect/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'mainproyect.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 # Password validation
